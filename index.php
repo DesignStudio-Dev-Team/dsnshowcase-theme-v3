eac926bf7e6aef3f60a243476e3f27ad2e221d65
markkingDS <74937599+markkingDS@users.noreply.github.com>
2025-01-05 20:39:08 -0500

    foundation

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<main>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <div><?php the_content(); ?></div>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p>No content found</p>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
