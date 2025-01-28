9391bc03cb4a1a25be017114a632ae87d406389d
dspuneet <126902089+dspuneet@users.noreply.github.com>
2025-01-28 17:50:27 +0530

    header1 update

--- templates/header1.php
+++ templates/header1.php
@@ -131,7 +131,7 @@ $header_logo = get_field('header_logo', 'options');
 				});
 				
 				var clickable_desktop = $('.util-left-nav ul').attr('data-clickable');
-				$('.util-left-nav ul li:has(ul)').addClass('has-sub dsn-relative dsn-flex dsn-items-center dsn-gap-2');
+				$('.util-left-nav ul li:has(ul)').addClass('has-sub dsn-relative dsn-flex dsn-items-center dsn-gap-2 dsn-cursor-pointer');
 				$('.util-left-nav ul .has-sub>a').after('<span class="dsn_nav__caret dsn-text-[#0988c2] dsn-rotate-90"><svg fill="currentColor" width="20" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></span>');
 
               }(jQuery));
