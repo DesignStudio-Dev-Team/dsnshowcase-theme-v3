1f96c8ef1b4f0e71ca88e688f32daf777518a486
dspuneet <126902089+dspuneet@users.noreply.github.com>
2025-02-05 20:34:53 +0530

    Header one changes | Search radio buttons

--- templates/mobile-header.php
+++ templates/mobile-header.php
@@ -1,8 +1,8 @@
-<header class="mobile block dsn:md:hidden dsn:text-[#0988c2] dsn:border-b">
-	<div class="dsn:flex dsn:justify-between dsn:px-4 dsn:py-4">
+<header class="mobile block dsn:lg:hidden dsn:text-[#0988c2] dsn:border-b">
+	<div class="dsn:flex dsn:justify-between dsn:px-4 dsn:py-4 dsn:container">
 		<div class="dsn-mobile-logo dsn:w-1/2"><a class="dsn:block dsn:relative dsn:text-center" href="<?php
             echo esc_url(home_url('/'));
-        ?>"><img class="dsn:w-48 dsn:md:w-64 dsn:mx-auto" src="<?php echo $header_logo['url']; ?>" alt="<?php bloginfo('name'); ?>" /> <span class="dsn:hidden"> <?php bloginfo('name'); ?></span></a></div>
+        ?>"><img class="dsn:w-48" src="<?php echo $header_logo['url']; ?>" alt="<?php bloginfo('name'); ?>" /> <span class="dsn:hidden"> <?php bloginfo('name'); ?></span></a></div>
 		<div class="dsn-mobile-icons dsn:flex dsn:items-center dsn:justify-end dsn:gap-3 dsn:w-1/2">
 			<a class="the-search-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="dsn:stroke-current dsn:fill-current dsn:cursor-pointer" width="22" height="22">
 			<path class="dsn:stroke-current dsn:fill-current" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></a>
