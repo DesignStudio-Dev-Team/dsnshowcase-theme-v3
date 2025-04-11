455dfa18fda23bee7e579064f6f363f0c6a2632b
Juan Tamayo <juan@designstudio.com>
2025-04-10 17:00:01 -0700

    update on blocks for heroes

--- inc/customblocks/parallax-block.php
+++ inc/customblocks/parallax-block.php
@@ -1,20 +1,81 @@
 <?php 
 $parallax = get_field('parallax_block', $block_id);
-$bgImg = $parallax['background_image'];
-$logo = $parallax['logo'];
-$title = $parallax['title'];
-$description = $parallax['description'];
-$cta = $parallax['cta'];
+$visualElement = $parallax['visual_element'];
+$bgType = $visualElement['background_type'];
+$bgImg = $visualElement['background_image'];
+$videoLink = $visualElement['video_link'];
+$container = $visualElement['container'];
+
+if($container == 'full-width') {
+    $containerClass = 'dsn:mx-auto';
+} else {
+    $containerClass = 'dsn:container dsn:mx-auto dsn:relative';
+}
+
+$content = $parallax['content'];
+$logo = $content['logo'];
+$title = $content['title'];
+$description = $content['description'];
+$cta = $content['cta'];
+$position = $content['position'];
+
+if($position == 'top-left') {
+      $positionClass = 'dsn:items-start dsn:justify-start';
+}
+if($position == 'top-center') {
+      $positionClass = 'dsn:items-center dsn:justify-start';
+}
+if($position == 'top-right') {
+      $positionClass = 'dsn:items-end dsn:justify-start';
+}
+if($position == 'middle-left') {
+      $positionClass = 'dsn:items-start dsn:justify-center';
+}
+if($position == 'middle-center') {
+      $positionClass = 'dsn:items-center dsn:justify-center';
+}
+if($position == 'middle-right') {
+      $positionClass = 'dsn:items-end dsn:justify-center';
+}
+if($position == 'bottom-left') {
+      $positionClass = 'dsn:items-start dsn:justify-end';
+}
+if($position == 'bottom-center') {
+      $positionClass = 'dsn:items-center dsn:justify-end';
+}
+if($position == 'bottom-right') {
+      $positionClass = 'dsn:items-end dsn:justify-end';
+}
+
+
 ?>
 
-<section  class="dsn:my-10">
+<section  class="dsn:my-10 <?php echo $containerClass; ?>">
+<?php if($bgType == 'image') { ?>
 <div class="dsn:h-[1000px]" style="background: url('<?php echo $bgImg; ?>') no-repeat center center; background-size: cover;">
-      <div class="dsn:container dsn:mx-auto dsn:relative dsn:flex dsn:flex-col dsn:items-start dsn:justify-center dsn:mr-20 dsn:h-full">
-         <div class="dsn:p-0 dsn:mt-50 dsn:w-full dsn:md:w-1/4 ">   
-               <img class="dsn:mb-5" src="<?php echo $logo; ?>" alt="logo">
+<?php } ?>
+<?php if($bgType == 'video') { ?>
+<div class="dsn:h-[1000px] dsn:relative">
+      <video class="dsn:w-full dsn:h-full dsn:object-cover dsn:absolute" autoplay muted loop>
+            <source src="<?php echo $videoLink; ?>" type="video/mp4">
+            Your browser does not support the video tag.
+      </video>
+      <div class="dsn:absolute dsn:top-0 dsn:left-0 dsn:w-full dsn:h-full dsn:bg-black dsn:opacity-40"></div>
+<?php } ?>
+      <div class="dsn:container dsn:mx-auto dsn:relative dsn:flex dsn:flex-col dsn:p-20 dsn:h-full <?php echo $positionClass; ?>">
+         <div class="dsn:w-full dsn:md:w-1/4 ">   
+            <?php if($logo) { ?>
+            <img class="dsn:mb-5" src="<?php echo $logo; ?>" alt="logo">
+            <?php } ?>
+            <?php if($title) { ?>
                <h2 class="dsn:text-6xl dsn:text-white"><?php echo $title; ?></h2>
+            <?php } ?>
+            <?php if($description) { ?>
                <p class="dsn:text-white dsn:mb-10"><?php echo $description; ?></p>
+            <?php } ?>
+            <?php if($cta) { ?>
                <a class="btn dsn:mt-15 dsn:justify-start dsn:self-start" href="<?php echo $cta['url']; ?>" role="button"><?php echo $cta['title']; ?></a>
+            <?php } ?>
          </div>
       </div>
 </div>
