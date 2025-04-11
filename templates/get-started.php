509921a722e582ca31020a40460221fc425aff72
dspuneet <126902089+dspuneet@users.noreply.github.com>
2025-04-11 22:16:14 +0530

    get started Changes

--- templates/get-started.php
+++ templates/get-started.php
@@ -1,11 +1,11 @@
 <div class="get-started-wrapper dsn:fixed dsn:flex-col dsn:w-full dsn:h-full dsn:top-0 dsn:left-0 dsn:flex dsn:justify-center dsn:items-center dsn:bg-[#000]/95 dsn:z-60" style="display: none;">
-        <div class="esc-get-started-form dsn:text-right dsn:w-2/3 dsn:lg:w-1/2 dsn:h-10 dsn:flex dsn:justify-end dsn:items-center"><a href="javascript:;" class="close-get-started dsn:w-15 dsn:h-15 dsn:text-4xl dsn:leading-0 dsn:pb-2 dsn:text-white dsn:before:absolute dsn:before:top-5 dsn:after:top-5 dsn:before:w-8 dsn:before:h-1 dsn:before:bg-white dsn:before:rounded-xs dsn:before:top-7 dsn:after:absolute dsn:after:w-8 dsn:after:h-1 dsn:after:bg-white dsn:after:rounded-xs dsn:after:top-7 dsn:before:rotate-45 dsn:before:right-3 dsn:after:right-3 dsn:after:-rotate-45 dsn:relative dsn:bg-black dsn:rounded-full dsn:-mb-10 dsn:-mr-10 get-started-close" role="button"></a></div>
-			<div class="get-started-inner dsn:flex dsn:flex-col dsn:lg:flex-row dsn:items-center dsn:w-2/3 dsn:lg:w-1/2 dsn:bg-white dsn:min-h-auto dsn:md:min-h-[500px]">
+        <div class="esc-get-started-form dsn:text-right dsn:w-2/3 dsn:lg:w-1/2 dsn:h-10 dsn:flex dsn:justify-end dsn:items-center"><a href="javascript:;" class="close-get-started dsn:w-15 dsn:h-15 dsn:text-4xl dsn:leading-0 dsn:pb-2 dsn:text-white dsn:before:absolute dsn:before:top-5 dsn:after:top-5 dsn:before:w-8 dsn:before:h-1 dsn:before:bg-white dsn:before:rounded-xs dsn:before:top-6 dsn:after:absolute dsn:after:w-8 dsn:after:h-1 dsn:after:bg-white dsn:after:rounded-xs dsn:after:top-6 dsn:before:rotate-45 dsn:before:right-2.5 dsn:after:right-2.5 dsn:after:-rotate-45 dsn:relative dsn:bg-black dsn:rounded-full dsn:-mb-10 dsn:-mr-7 get-started-close" role="button"></a></div>
+			<div class="get-started-inner dsn:flex dsn:flex-col dsn:lg:flex-row dsn:items-center dsn:w-2/3 dsn:lg:w-1/2 dsn:bg-white dsn:min-h-auto dsn:md:min-h-[70vh]">
                 <div class="dsn:w-full dsn:lg:w-5/12 dsn:h-40 dsn:lg:h-full dsn:p-4 dsn:md:p-6 dsn:flex dsn:flex-col dsn:justify-end dsn:!bg-cover dsn:!bg-center dsn:relative" style="background: url(https://valleyhotspring.designstudio.host/wp-content/uploads/2024/12/cal-spas-deck-spa-series.png);">
                     <div class="overlay dsn:absolute dsn:left-0 dsn:top-0 dsn:w-full dsn:h-full dsn:bg-linear-to-b dsn:from-transparent dsn:to-black dsn:from-60% dsn:opacity-80"></div>
                    <h2 class="dsn:text-white dsn:text-lg dsn:xl:text-3xl dsn:z-10 dsn:mb-0 ">Let's Build Your<br />Backyard Sanctuary!</h2>
                 </div>
-                <div class="dsn:bg-white dsn:p-8 dsn:mx-auto dsn:w-full dsn:lg:w-7/12">
+                <div class="dsn:bg-white dsn:py-8 dsn:px-14 dsn:md:py-15 dsn:md:px-25 dsn:mx-auto dsn:w-full dsn:lg:w-7/12">
                     <?php echo do_shortcode('[gravityform id="125" title="false" description="false" ajax="true"]'); ?>
                 </div>
                 </div>
@@ -13,11 +13,11 @@
         </div>
 <style>
     .get-started-inner {
-        box-shadow: 0px 0px 10px 0px #ffffff;
+        border: 3px solid #fff;
         border-radius: 10px;
         overflow: hidden;
     }
     .get-started-close {
-        box-shadow: 0px 0px 5px 0px #ffffff;
+        border: 3px solid #fff;
     }
 </style>
