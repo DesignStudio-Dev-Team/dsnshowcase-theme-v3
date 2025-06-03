8850257f34709127646e86eafd26e0627fbeb791
dspuneet <126902089+dspuneet@users.noreply.github.com>
2025-06-03 23:14:06 +0530

    Custom Block | Logo Slider | Product Showcase

--- assets/js/custom-blocks-slider.js
+++ assets/js/custom-blocks-slider.js
@@ -15,6 +15,8 @@ jQuery(document).ready(function ($) {
         pauseOnFocus: true,     // Pauses autoplay on focus
     });
   }
+
+  //outdoor internal page bottom slider
   if ($('.outdoor-kitchen-gallery').length > 0) {
     $('.outdoor-kitchen-gallery').slick({
         centerMode: true,       // Enables center mode
@@ -62,6 +64,129 @@ jQuery(document).ready(function ($) {
         navigateByKeyboard  : true
     });
   }
+
+  //Custom block logo slider
+  if ($('.logo-slider').length > 0) {   
+    $('.logo-slider').slick({
+        rows: 2,
+        slidesToShow: 3,        // Displays only one slide at a time
+        slidesToScroll: 1,      // Scrolls one slide at a time
+        infinite: true,         // Enables infinite looping
+        arrows: true,           // Shows navigation arrows
+        dots: false,             // Shows navigation dots
+        autoplay: true,         // Enables automatic sliding
+        autoplaySpeed: 5000,    // Time in milliseconds between slides
+        pauseOnHover: true,     // Pauses autoplay on hover
+        pauseOnFocus: true,     // Pauses autoplay on focus
+        responsive: [           //Responsive Breakpoint settings
+            {
+              breakpoint: 1024, 
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false, 
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600, 
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 480, 
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false,
+              }
+            }]
+    });
+  }
+//Product Showcase Tab Slider 
+
+  var $carousel = $('.slider-for').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: false,
+   fade: true,
+   initialSlide: 0,
+ });
+ 
+ $cat_val = $('.slider-nav').slick({
+   slidesToShow: 8,
+   slidesToScroll: 1,
+   asNavFor: '.slider-for',
+   dots: false,
+   initialSlide: 0,
+   focusOnSelect: true,
+   centerMode: false,
+   responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1.7,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1.7,
+        }
+      }
+    ]
+ });
+ var select = $("#category");
+  $("#category").change( function() { 
+if (select.prop('selectedIndex')) {
+    goTo = select.prop('selectedIndex');
+} else {
+	goTo = 0;
+}
+    $carousel.slick( "goTo", goTo-1 );
+    $cat_val.slick( "goTo", goTo-1 );
+  });
+ 
+
+ $('a[data-slide]').click(function(e) {
+   e.preventDefault();
+   var slideno = $(this).data('slide');
+   $('.slider-nav').slick('slickGoTo', slideno - 1);
+ });
+
+
+//Tab Slider
+$('.all-products').on('init', function(event, slick){
+        $(this).append('<div class="slider-count dsn:w-max dsn:mx-auto dsn:mt-10"><p><span id="current">1</span> / <span id="total">'+slick.slideCount+'</span></p></div>');
+    });
+ var $slider = $('.all-products');
+  var $progressBar = $('.progress');
+  var $progressBarLabel = $( '.slider__label' );
+  
+  $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
+    var calc = ( (nextSlide) / (slick.slideCount-1) ) * 100;
+    
+    $progressBar
+      .css('background-size', calc + '% 100%')
+      .attr('aria-valuenow', calc );
+    
+    $progressBarLabel.text( calc + '% completed' );
+  });
+  
+  $slider.slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    speed: 400
+  });  
+      $slider
+        .on('afterChange', function(event, slick, currentSlide, nextSlide){
+            // finally let's do this after changing slides
+            $('.slider-count #current').html(currentSlide+1);
+        });
+  
 });
+
 
   
--- inc/customblocks/products-showcase-block.php
+++ inc/customblocks/products-showcase-block.php
@@ -6,7 +6,129 @@ $titleOfShowcase = $productsShowcaseBlock['title_of_showcase'];
 $set_of_products = $productsShowcaseBlock['set_of_products'];
 ?>
 
-<section class="dsn:container dsn:mx-auto dsn:my-10">
-    <h2 class="dsn:text-center dsn:mb-10"><?php echo $title; ?></h2>
-  
+<section id="product-showcase" class="dsn:my-10 dsn:p-10 dsn:text-white">
+    <div class="dsn:container dsn:mx-auto dsn:relative">
+    <h2 class="dsn:text-left dsn:mb-10"><?php echo $title; ?></h2>
+    <p class="dsn:text-left dsn:lg:max-w-8/12"><?php echo $description; ?></p>
+    <div class="product-showcase-inner dsn:pt-10">
+    
+<div class="slider slider-nav dsn:mb-8 dsn:py-4 dsn:border-b-2 dsn:border-white">
+
+    <?php 
+    $i = 0;
+    //print_r($set_of_products);
+foreach($set_of_products as $category => $value ) {
+
+   echo '<div class="slide-nav-label dsn:relative" data-slide='. $i .'>' . $value['category_name'] . '</div>';
+   $i++;
+}
+?>
+  </div>
+  <div class="slider slider-for">
+    <?php 
+
+    $j = 0;
+
+	foreach($set_of_products as $category => $value ) { 
+        $category = $value['products'];
+         $j++;
+    
+?>
+
+<div class="all-products dsn:w-full" data-slide="<?php echo $j; ?>"> <?php
+ if (!empty($set_of_products)): ?>
+                <?php foreach ($category as $post):
+                    setup_postdata($post); 
+                    $product = wc_get_product( get_the_id() );
+                    print_r($product);
+                    ?>
+                    <div class="dsn:w-full dsn:mx-3 dsn:md:mx-4 dsn:mb-4 dsn:bg-white dsn:shadow-lg dsn:p-6">
+                        <a href="<?php echo get_permalink(); ?>">
+                            <div class="dsn:text-center thumbnail">
+                                <?php if (has_post_thumbnail()): ?>
+                                    <?php the_post_thumbnail(); ?>
+                                <?php else: ?>
+                                    <?php echo wc_placeholder_img(); ?>
+                                <?php endif; ?>
+                            </div>
+                            <p class="dsn:text-black dsn:mb-0 dsn:uppercase dsn:text-lg product-category"><?php echo $value['category_name']; ?></p>
+                            <h3 class="dsn:text-left dsn:text-white product-title"><?php the_title(); ?></h3>
+                        </a>
+                        <div class="product-bottom dsn:flex dsn:item-center"><span class="price"><?php //echo get_price(); ?></span><span class="cart-btn"></span></div>
+                    </div>
+                <?php endforeach; ?>
+    <?php endif; ?>
+<?php //reset query
+wp_reset_postdata();
+
+?>
+
+  </div>
+   
+  <?php } ?>
+ 
+  </div>
+   <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+    <span class="slider__label sr-only"></span>
+</div>
+</div>
 </section>
+
+<style>
+    #product-showcase {
+    background: #286632; 
+}
+.product-title {
+    color: #286632;
+}
+    .progress {
+    display: block;
+    width: 85%;
+    height: 10px;
+    border-radius: 10px;
+    overflow: hidden;
+    background-color: #f5f5f5;
+    background-image: linear-gradient(to right, black, black);
+    background-repeat: no-repeat;
+    background-size: 0 100%;
+    transition: background-size .4s ease-in-out;
+    position: absolute;
+    left: 0;
+    bottom: 1em;
+    }
+    .slider-count {
+        position: absolute;
+        right: 0;
+        bottom: -20px;
+    }
+    .all-products {
+        padding-bottom: 4em;
+    }
+   
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0,0,0,0);
+  border: 0;
+}
+  .thumbnail img {
+    margin-left: auto;
+    margin-right: auto;
+    }
+    .slider-nav .slick-track {
+        margin-left: 0;
+    }
+    .slide-nav-label.slick-slide.slick-current.slick-active:after {
+    content: "";
+    width: 100%;
+    height: 4px;
+    background: red;
+    position: absolute;
+    bottom: -5px;
+    z-index: 9999;
+}
+</style>
