d8493847a379fdde4fd6cc861c92c93646519abe
markkingDS <74937599+markkingDS@users.noreply.github.com>
2025-06-10 21:52:24 -0400

    Update nav-items.php

--- inc/nav-items.php
+++ inc/nav-items.php
@@ -31,7 +31,7 @@ function dsn_add_class_nav_primary( $atts, $item, $args ) {
 }
 
 function add_menu_link_class( $atts, $item, $args ) {
-  if (property_exists($args, 'link_class')) {
+  if (is_object($args) && property_exists($args, 'link_class')) {
     $atts['class'] = $args->link_class;
   }
   return $atts;
