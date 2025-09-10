e5d547f4e7c1293ed7fb12e017fd734309ba42c8
dspuneet <126902089+dspuneet@users.noreply.github.com>
2025-09-10 18:02:33 +0530

    reduced title and grid brand spacing

--- inc/customblocks/grid-brands.php
+++ inc/customblocks/grid-brands.php
@@ -6,7 +6,7 @@ $brands = $gridBrands['brands'];
 ?>
 
 
-<section id="gridBrands-<?php echo $block_id; ?>" class="dsn:py-20 dsn:px-0">
+<section id="gridBrands-<?php echo $block_id; ?>" class="dsn:pb-20 dsn:px-0">
     <div class="dsn:text-left dsn:md:text-center dsn:mb-15">
         <h2><?php echo $title; ?></h2>
     </div>
