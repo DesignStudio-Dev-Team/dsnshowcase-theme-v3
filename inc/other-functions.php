6a82166767dbec270a706af6c37d3d507953eaa7
Renzo <renzo.castillo@corp.designstudio.com>
2025-09-17 17:18:13 -0500

    wip

--- inc/other-functions.php
+++ inc/other-functions.php
@@ -318,21 +318,29 @@ function ds_filtration($categories = null, $specials = null, $featured_image = n
                 $the_query->the_post(); ?>
                 <?php $product = wc_get_product(get_the_ID()); ?>
                 <div class="dsn:w-full dsn:sm:w-1/2 dsn:md:w-1/3 dsn:px-4 dsn:mb-12">
-                    <div class="ds-product">
+                    <div class="ds-product dsn:bg-white dsn:p-5">
                         <a href="<?php echo get_permalink() ?>">
-                            <?php if ($product->is_on_sale()) : ?>
-                                <span class="ds-product__sale">Sale</span>
-                            <?php endif; ?>
-                            <span class="ds-product__image" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">
-                                <?php if ($product->get_price_html()) { ?>
-                                    <button class="single_add_to_cart_button dsw-primary-site-background" value="<?php echo get_the_ID(); ?>">
-                                        <i class="fas fa-shopping-cart"></i>
-                                        <i class="fas fa-spinner"></i>
-                                        <i class="far fa-check-circle"></i>
-                                    </button>
-                                <?php } ?>
-                            </span>
-                            <span class="ds-product__title"><?php the_title(); ?></span>
+                          <?php if ($product->is_on_sale()) : ?>
+                              <span class="ds-product__sale">Sale</span>
+                          <?php endif ?>
+
+                          <span class="ds-product__image" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">
+                              <?php if ($product->get_price_html()) { ?>
+                                  <button class="single_add_to_cart_button dsw-primary-site-background" value="<?php echo get_the_ID(); ?>">
+                                    <svg xmlns='http://www.w3.org/2000/svg'
+                                           fill='none' viewBox='0 0 24 24'
+                                           stroke-width='1.5'
+                                           stroke='currentColor'
+                                           class='size-6'>
+                                          <path stroke-linecap='round' stroke-linejoin='round' d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'/>
+                                      </svg>
+                                      <i class="fas fa-shopping-cart"></i>
+                                      <i class="fas fa-spinner"></i>
+                                      <i class="far fa-check-circle"></i>
+                                  </button>
+                              <?php } ?>
+                          </span>
+                          <span class="ds-product__title"><?php the_title(); ?></span>
                         </a>
                         <div class="ds-product__meta">
                             <div class="ds-product__price"><?php echo $product->get_price_html(); ?></div>
