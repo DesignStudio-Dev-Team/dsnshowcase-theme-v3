abdb231ac4c41e71b8ceb7a11cda90501991c0b5
Renzo <renzo.castillo@corp.designstudio.com>
2025-09-18 16:29:33 -0500

    Adds icon functionality and replaces cart icon
    Introduces a new function to handle icons, streamlining their usage throughout the theme.
    
    Replaces the inline SVG and Font Awesome implementation of the shopping cart icon with the new icon function.

---
+++ inc/icons.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'shopping-cart' => '<svg class="%s" fill="currentColor" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="shopping-cart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
+        <path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"></path>
+    </svg>',
+    
+    'plus' => '<svg class="%s" fill="currentColor" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" data-fa-i2svg="">
+        <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z"/>
+    </svg>',
+    
+    'spinner' => '<svg class="%s" fill="currentColor" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="spinner" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+        <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/>
+    </svg>',
+    
+    'check-circle' => '<svg class="%s" fill="currentColor" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="check-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
+    </svg>'
+);
--- inc/other-functions.php
+++ inc/other-functions.php
@@ -327,16 +327,8 @@ function ds_filtration($categories = null, $specials = null, $featured_image = n
                           <span class="ds-product__image" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">
                               <?php if ($product->get_price_html()) { ?>
                                   <button class="single_add_to_cart_button dsw-primary-site-background" value="<?php echo get_the_ID(); ?>">
-                                    <svg xmlns='http://www.w3.org/2000/svg'
-                                           fill='none' viewBox='0 0 24 24'
-                                           stroke-width='1.5'
-                                           stroke='currentColor'
-                                           class='size-6'>
-                                          <path stroke-linecap='round' stroke-linejoin='round' d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'/>
-                                      </svg>
-                                      <i class="fas fa-shopping-cart"></i>
-                                      <i class="fas fa-spinner"></i>
-                                      <i class="far fa-check-circle"></i>
+                                    <?php     dsn_icon('shopping-cart', 'dsw-w-5 dsw-h-5'); ?>
+
                                   </button>
                               <?php } ?>
                           </span>
@@ -1362,4 +1354,24 @@ function format_order_for_wpml( $value ){
         $value[$key] = apply_filters( 'wpml_object_id', $id, $type, true, $lang);
     }
     return $value;
+}
+
+function dsn_icon($icon_name, $classes = '') {
+    static $icons = null;
+    
+    // Load icons only once per page load
+    if ($icons === null) {
+        $icons = include get_template_directory() . '/inc/icons.php';
+    }
+    
+    if (!isset($icons[$icon_name])) {
+        echo '<!-- Icon "' . esc_attr($icon_name) . '" not found -->';
+        return;
+    }
+    
+    if (empty($classes)) {
+        $classes = 'dsn-icon';
+    }
+    
+    echo sprintf($icons[$icon_name], esc_attr($classes));
 }
