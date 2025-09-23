0414bf33e37d53633a11a426accafce7def148ea
Renzo <renzo.castillo@corp.designstudio.com>
2025-09-23 14:21:24 -0500

    wip

--- inc/icons.php
+++ inc/icons.php
@@ -19,6 +19,6 @@ return array(
 
     'enter' => '<svg class="%s" viewBox="0 0 24 24" fill="none" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg">
         <path d="M9 4h10v14a2 2 0 0 1-2 2H9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12 15l3-3-3-3M15 12H5" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12 15l3-3-3-3M15 12H5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>',
 );
