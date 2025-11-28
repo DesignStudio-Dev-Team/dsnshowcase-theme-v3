11a0fb722145dd364bc3117571e6c3c2e7be8d88
Renzo <renzo.castillo@corp.designstudio.com>
2025-11-28 14:37:52 -0500

    Improves layout of variation add to cart
    Enhances the structure and spacing of the variation add-to-cart section, including quantity input and "Get a Quote" button.
    This change provides a more visually appealing and user-friendly experience.

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -13,23 +13,29 @@ global $product;
 ?>
 <div class="woocommerce-variation-add-to-cart variations_button">
 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
-    <label for="quantity" class="mr-4">Qty</label>
-	<?php
-	do_action( 'woocommerce_before_add_to_cart_quantity' );
+	
+	<div class="dsn:flex dsn:flex-wrap dsn:items-center dsn:gap-4 dsn:mb-4">
+		<div class="dsn:flex dsn:items-center dsn:gap-2">
+			<label for="quantity" class="dsn:mb-0">Qty</label>
+			<?php
+			do_action( 'woocommerce_before_add_to_cart_quantity' );
 
-	woocommerce_quantity_input(
-		array(
-			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-		)
-	);
+			woocommerce_quantity_input(
+				array(
+					'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+					'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+					'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				)
+			);
 
-	do_action( 'woocommerce_after_add_to_cart_quantity' );
-	?>
+			do_action( 'woocommerce_after_add_to_cart_quantity' );
+			?>
+		</div>
 
-	<button type="submit" class="single_add_to_cart_button button alt disabled"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-    <a href="#" class="button alt">Get a Quote</a>
+		<button type="submit" class="single_add_to_cart_button button alt disabled"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		<a href="#" class="button alt">Get a Quote</a>
+	</div>
+
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 
 	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
